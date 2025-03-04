2a1d3c4e397490fa3a0fb64bc5ac41f27a2e6def
alexmvalencia <alexmvalencia@vt.edu>
2025-03-04 00:25:57 -0500

    deleted

--- ros_entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Source ROS 2 jazzy setup files
-source /opt/ros/jazzy/setup.bash
-#source /ros_ws/install/setup.bash
-
-# Execute the command passed to the container
-exec "$@"
